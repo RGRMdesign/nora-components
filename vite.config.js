c6574645a2f2689ea28d01a75b11ee148cf2f68b
Rogier Mechielsen <mail@rogiermechielsen.nl>
2025-02-10 12:38:09 +0100

    refactor and add build files

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        'mw-nora-components': path.resolve(__dirname, 'resources/css/mw-nora-components.scss'),
+        'mw-custom': path.resolve(__dirname, 'resources/css/mw-custom.scss')
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.names[0])) {
+            return 'assets/fonts/[name][extname]'; // Fonts gaan nu in dist/assets/fonts/
+          }
+          return '[name][extname]';
+        }
+      }
+    }
+  }
+});
